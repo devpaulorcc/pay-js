f1c3134dcdfe460dcea94ec018c9762fb4365903
devpaulorcc <dev.paulorc@gmail.com>
2025-05-17 18:44:08 -0300

    feat: middleware to validate access token created by server

---
+++ middleware/auth.ts
@@ -0,0 +1,7 @@
+export default defineNuxtRouteMiddleware((to, from) => {
+  const token = useCookie('accessToken')
+
+  if (!token.value) {
+    return navigateTo('/login')
+  }
+})
--- pages/auth/api.vue
+++ pages/auth/api.vue
@@ -1,3 +1,8 @@
 <template>
   <Dashboard />
 </template>
+
+<script setup lang='ts'>
+definePageMeta({
+  middleware: 'auth'
+})</script>
